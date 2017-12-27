8363df0356c20c92f6dae175b3c8b83b1032e6ff
Tasuku Suzuki <tasuku.suzuki@qt.io>
2017-12-27 20:34:03 +0900

    Add basic translation support
    Added French and Japanese translations
    
    Change-Id: I58eca5ffaad2367a4dd26b6472bdbcd45c957061
    Signed-off-by: Tasuku Suzuki <tasuku.suzuki@qt.io>
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

---
+++ app/translations/hvac_en_US.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>HVAC</name>
+    <message>
+        <location filename="../HVAC.qml" line="66"/>
+        <source>FAN SPEED</source>
+        <translation>FAN SPEED</translation>
+    </message>
+    <message>
+        <location filename="../HVAC.qml" line="98"/>
+        <source>A/C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../HVAC.qml" line="111"/>
+        <source>AUTO</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>HeatDegree</name>
+    <message>
+        <location filename="../HeatDegree.qml" line="31"/>
+        <source>LO</source>
+        <translation>LO</translation>
+    </message>
+    <message>
+        <location filename="../HeatDegree.qml" line="33"/>
+        <source>%1°</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../HeatDegree.qml" line="35"/>
+        <source>HI</source>
+        <translation>HI</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="44"/>
+        <source>port for binding</source>
+        <translation>Port for binding</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="45"/>
+        <source>secret for binding</source>
+        <translation>Secret token for binding</translation>
+    </message>
+</context>
+</TS>
